e034631f6fee1981ec1625291ea67967e0304c32
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2019-07-01 22:22:05 +0200

    Implement the campaign discrapency detector service
    * This service gets a local and remote campaigns and returns an object of the discrapencies

--- lib/services/campaign_discrapencies_detector.rb
+++ lib/services/campaign_discrapencies_detector.rb
@@ -1,5 +1,48 @@
 # frozen_string_literal: true
 
 class CampaignDiscrapenciesDetector
-  def self.call(local_campaign:, remote_ad:); end
+  ATTRIBUTES_TO_COMPARE = %i[status description].freeze
+
+  def self.call(local_campaign:, remote_ad:)
+    new(local_campaign, remote_ad).call
+  end
+
+  def initialize(local, remote)
+    @local = local
+    @remote = remote
+  end
+
+  def call
+    return if discrepant_attributes.empty?
+
+    formatted_output
+  end
+
+  private
+
+  attr_reader :local, :remote
+
+  def discrepant_attributes
+    @discrepant_attributes ||= ATTRIBUTES_TO_COMPARE.reject do |attr|
+      local.send(attr) == remote.send(attr)
+    end
+  end
+
+  def formatted_output
+    {
+      'remote_reference' => local.external_reference,
+      'discrepancies' => formatted_discrapencies
+    }
+  end
+
+  def formatted_discrapencies
+    discrepant_attributes.map do |attr|
+      {
+        attr.to_s => {
+          'remote' => remote.send(attr),
+          'local' => local.send(attr)
+        }
+      }
+    end
+  end
 end
---
+++ spec/services/campaign_discrapencies_detector_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe CampaignDiscrapenciesDetector do
+  describe '::call' do
+    let(:local_campaign) do
+      instance_double(
+        LocalCampaign,
+        external_reference: 'reference',
+        status: local_status,
+        description: local_description
+      )
+    end
+
+    let(:remote_ad) do
+      instance_double(
+        RemoteAd,
+        status: remote_status,
+        description: remote_description
+      )
+    end
+
+    subject do
+      described_class.call(
+        local_campaign: local_campaign,
+        remote_ad: remote_ad
+      )
+    end
+
+    context 'when there are no discrepancies' do
+      let(:local_status) { 'status' }
+      let(:remote_status) { 'status' }
+      let(:local_description) { 'description' }
+      let(:remote_description) { 'description' }
+
+      it { is_expected.to be_nil }
+    end
+
+    context 'when there are discrepancies' do
+      let(:local_status) { 'status_1' }
+      let(:remote_status) { 'status_2' }
+      let(:local_description) { 'description_1' }
+      let(:remote_description) { 'description_2' }
+
+      let(:expected_output) do
+        {
+          'remote_reference' => 'reference',
+          'discrepancies' => [
+            {
+              'status' => {
+              'remote' => 'status_2',
+              'local' => 'status_1'
+              }
+            },
+            {
+              'description' => {
+                'remote' => 'description_2',
+                'local' => 'description_1'
+              }
+            }
+          ]
+        }
+      end
+
+      it { is_expected.to eq expected_output }
+    end
+  end
+end
