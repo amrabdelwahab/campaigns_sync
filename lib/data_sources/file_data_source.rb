efeb8eefde1a62c3aedf07c4fcdc2e60b06cd6fa
Amr Abdelwahab <amr.mohamed.abdelwahab@gmail.com>
2019-07-01 21:23:38 +0200

    Implement file_data_source class
    * This data source reads the data from a json file and wraps it in a value object

--- lib/data_sources/file_data_source.rb
+++ lib/data_sources/file_data_source.rb
@@ -1,7 +1,31 @@
 # frozen_string_literal: true
 
-class FileDataSource
-  def initialize(data_key:, file_name:, model_class:); end
+require 'json'
 
-  def all; end
+class FileDataSource
+  attr_reader :model_class
+
+  def initialize(data_key:, file_name:, model_class:)
+    @data_key = data_key
+    @file_name = file_name
+    @model_class = model_class
+  end
+
+  def all
+    content.map do |item|
+      model_class.new(item)
+    end
+  end
+
+  private
+
+  attr_reader :data_key, :file_name
+
+  def content
+    JSON.parse(raw_content).fetch(data_key, [])
+  end
+
+  def raw_content
+    @raw_content ||= File.open("./storage/#{file_name}", 'r', &:read)
+  end
 end
---
+++ spec/data_sources/file_data_source_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe FileDataSource do
+  let(:data_key) { 'data_key' }
+  let(:file_name) { 'file_name.json' }
+  let(:model_class) { class_double('Model') }
+
+  let(:data_source) do
+    described_class.new(
+      data_key: data_key,
+      file_name: file_name,
+      model_class: model_class
+    )
+  end
+
+  describe '#all' do
+    subject { data_source.all }
+    let(:item) { { 'attr_1' => 'val_1' } }
+    let(:item_object) { double('Item') }
+    let(:data) do
+      {
+        'data_key' => [item]
+      }.to_json
+    end
+
+    before do
+      allow(File)
+        .to receive(:open)
+        .with('./storage/file_name.json', 'r')
+        .and_yield(StringIO.new(data))
+
+      allow(model_class)
+        .to receive(:new)
+        .with(item)
+        .and_return(item_object)
+    end
+
+    it { is_expected.to eq [item_object] }
+  end
+end
